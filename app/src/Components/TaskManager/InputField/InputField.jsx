5c8bbc95c35a7a36ddd490ef30f77bec6e7dbda7
Dark-Vol <overlord28985@gmail.com>
2025-06-13 18:19:39 +0300

    add create task

--- app/src/Components/TaskManager/InputField/InputField.jsx
+++ app/src/Components/TaskManager/InputField/InputField.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './InputField.module.scss';
 
 const InputField = ({ label, value, onChange, placeholder }) => (
-  <div>
-    <label className="">{label}</label>
+  <div className={styles.inputContainer}>
+    <label className={styles.LabelField}>{label}</label>
     <input
       type="text"
       value={value}
       onChange={onChange}
       placeholder={placeholder}
-      className=""
+      className={styles.inputField}
     />
   </div>
 );
--- app/src/Components/TaskManager/SubmitButton/SubmitButton.jsx
+++ app/src/Components/TaskManager/SubmitButton/SubmitButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './SubmitButton.module.scss';
 
 const SubmitButton = ({ text }) => (
   <button
     type="submit"
-    className=""
+    className={styles.submitButton}
   >
     {text}
   </button>
