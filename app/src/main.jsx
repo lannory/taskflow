614a430f3eb220321fa1b76722074bd4607c8378
alyona706 <almokina@gmail.com>
2025-06-22 11:34:42 +0300

    Resolved merge conflicts with main

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Provider} from 'react-redux';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import taskReduser from './store/Tasks/TasksSlice.js';
