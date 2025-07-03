32a9ba46b89ac8f4c141d4f8ce7147fe415a9542
Vybyranyi <mariankvit@gmail.com>
2025-07-03 10:55:24 +0300

    feat: add StatistickBlock component to display total completed tasks in Overview

--- app/src/Pages/Overview/Overview.jsx
+++ app/src/Pages/Overview/Overview.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import TodaysTask from "../../Components/TodaysTask/TodaysTask";
 import ProjectSummary from "../../Components/ProjectSummary/ProjectSummary";
 import styles from "./Overview.module.scss";
+import StatistickBlock from "../../Components/StatistickBlock/StatistickBlock";
 
 export default function Overview() {
     return (
         <div className={styles.OverviewWrapper}>
             <TodaysTask />
             <ProjectSummary />
+            <StatistickBlock type="total_tasks" />
         </div>
     )
 }
